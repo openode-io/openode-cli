7e580e3facdb51918a67b6c73038193232fdc80f
Martin Levesque <levesque.martin@gmail.com>
2017-07-03 18:24:56 -0400

    version detection

--- bin/index.js
+++ bin/index.js
@@ -2,4 +2,10 @@
 
 'use strict';
 
-require("../index");
+let version = process.version.replace("v", "");
+
+if (version < "7.6.0") {
+  console.error("openode: Please install node >= 7.6.0. If you have NVM, you can run \"nvm install 7.6.0\". \nYour current version is: " + version + ".");
+} else {
+  require("../index");
+}
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Spinner = require('cli-spinner').Spinner;
 
 function processCommander() {
   commander
-    .version('1.0.0');
+    .version('1.0.3');
 
   commander
     .command('deploy')
