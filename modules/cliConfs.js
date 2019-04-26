4258c304d717c7f5c4dd8d8f681767c09ae0a422
Martin Levesque <martin.levesque@ubisoft.com>
2019-04-26 13:17:01 -0400

    wip

--- modules/cliConfs.js
+++ modules/cliConfs.js
@@ -1,4 +1,19 @@
-module.exports = {
+
+const API_ENDPOINTS = [
+  "https://api.openode.io/",
+  "https://apifr.openode.io/"
+]
+
+const confs = {
   // "API_URL": "http://localhost:3180/"
-  "API_URL": "https://api.openode.io/"
+  "API_URL": API_ENDPOINTS[0]
+};
+
+async function determineClosestEndpoint() {
+
+}
+
+module.exports = {
+  API_URL: confs.API_URL,
+  determineClosestEndpoint
 }
