f58e82a836d2cd240b4a0611ef56102d23f2ffa5
Martin Levesque <levesque.martin@gmail.com>
2020-05-14 17:03:11 -0400

    dropping support for node < 8

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const compareVersion = require("compare-version");
 
 var version = process.version.replace("v", "");
 
-if (compareVersion(version, "7.6.0") == -1) {
+if (compareVersion(version, "8.0.0") == -1) {
   console.error("openode: Please install node >= 7.6.0. If you have NVM, you can run \"nvm install 7.6.0\". \nYour current version is: " + version + ".");
 } else {
   require("../index");
