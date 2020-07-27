a8fb7b221cb2344db44b55aa44ffb782fad0d817
Martin Levesque <levesque.martin@gmail.com>
2020-07-26 23:46:56 -0400

    node 10 minimum

--- bin/index.js
+++ bin/index.js
@@ -5,8 +5,8 @@ const compareVersion = require("compare-version");
 
 var version = process.version.replace("v", "");
 
-if (compareVersion(version, "8.0.0") == -1) {
-  console.error("openode: Please install node >= 8.0.0. If you have NVM, you can run \"nvm install 8.0.0\". \nYour current version is: " + version + ".");
+if (compareVersion(version, "10.0.0") == -1) {
+  console.error("openode: Please install node >= 10.0.0. If you have NVM, you can run \"nvm install 10.0.0\". \nYour current version is: " + version + ".");
 } else {
   require("../index");
 }
